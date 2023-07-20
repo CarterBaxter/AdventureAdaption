d6e6885d2ee6a3f9181f7953ca9eaafe95fe308f
CarterBaxter <CarterWBaxter@gmail.com>
2023-07-20 14:58:34 -0600

    Finished basic player movement and Jumping
    Fixed bug with basic movement and added friction along with adding jumping functionality. Again used some help from Youtube video referenced in last Push.

--- Adventure Adaptation v0.1/Assets/Scripts/PlayerController.cs
+++ Adventure Adaptation v0.1/Assets/Scripts/PlayerController.cs
@@ -5,36 +5,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("References")]
+    public LayerMask whatIsGround;
+
+    [Header("Variables")] //note playerRb weight is set to 80kg
+    //basic movement
     private Rigidbody playerRB;
-    public Vector3 moveDirection;
+    private Vector3 moveDirection;
+    private float vInput;
+    private float hInput;
+
+    private float playerHeight;
+    private bool grounded;
+
     public float speed = 5.0f;
-    public float vInput;
-    public float hInput;
+    public float groundDrag = 5.0f;
+
+    //Jumping
+    public float jumpForce = 600.0f;
+    public float jumpCD = .25f;
+    public float airMoveMultiplier = .4f;
+    public bool readyToJump = true;
+
+
+    [Header("KEYBINDS")]
+    public KeyCode jumpKey = KeyCode.Space;
 
 
 
-    
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         playerRB.freezeRotation = true;
+        playerHeight = GetComponent<BoxCollider>().size.y;
     }
 
     private void Update()
     {
-        DirectionalInput();
+        SpeedControl();
+        PlayerInput(); //get directional input
+        grounded = GroundCheck(); //check if grounded
+
+        //handle ground drag
+        if (grounded)
+        {
+            playerRB.drag = groundDrag; //drag on ground
+        }
+        else 
+        {
+            playerRB.drag = 0; //no drag in air
+        }
     }
 
     void FixedUpdate()
     {
-        DirectionalMove();  
+        DirectionalMove(); //move character based on PlayerInput in Update
     }
 
-    void DirectionalInput()
+
+
+
+    void PlayerInput()
     {
-        //get input
+        //get directional input
         vInput = Input.GetAxisRaw("Vertical");
         hInput = Input.GetAxisRaw("Horizontal");
+
+        //get jump input
+        if (Input.GetKey(jumpKey) && grounded && readyToJump)
+        { 
+            readyToJump = false;
+            Jump();
+            Invoke("ResetJump", jumpCD); //reset ready to jump after cooldown
+        }
     }
 
     void DirectionalMove() 
@@ -42,10 +85,50 @@ public class PlayerController : MonoBehaviour
         //get new move direction from transform and input
         moveDirection = transform.forward * vInput + transform.right * hInput;
         //add speed in that direction
-        playerRB.AddForce(moveDirection.normalized * speed * 10f, ForceMode.Force);
-
+        if (grounded)
+        {
+            playerRB.AddForce(moveDirection.normalized * speed * 800f, ForceMode.Force);
+        }
+        else //not on ground (harder to change directions in air)
+        {
+            playerRB.AddForce(moveDirection.normalized * speed * 800f * airMoveMultiplier, ForceMode.Force);
+        }
+        
 
     }
 
+    bool GroundCheck()
+    {
+        float raycastLength = playerHeight / 2 + .2f; //half the height, plus a little.
+
+        //Raycast( Vector3 Origin, Vector3 Position, float MaxDistance, int LayerMask)
+        //Returns true if hits collider with certain mask
+        return Physics.Raycast(transform.position, Vector3.down, raycastLength, whatIsGround);
+    }
+
+    void SpeedControl()
+    { 
+        Vector3 currVelocity = new Vector3(playerRB.velocity.x, 0f, playerRB.velocity.z);
+
+        if (currVelocity.magnitude > speed)
+        {
+            Vector3 ctrlVelocity = currVelocity.normalized * speed;
+            playerRB.velocity = new Vector3(ctrlVelocity.x, playerRB.velocity.y, ctrlVelocity.z);
+            //retain y verlocity but control x and z velocity to retain max speed.
+        }
+    }
+
+    void Jump()
+    {
+        //reset Y velocity so jumps stay consistent
+        playerRB.velocity = new Vector3(playerRB.velocity.x, 0f, playerRB.velocity.z);
+
+        playerRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
+    }
 
 }
